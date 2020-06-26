5d2e4a860befba034a6d785be0b5b52bda95ca4e
Rionn Kenshin Pineda <rionnken@outlook.com>
2020-06-26 16:03:30 +0800

    Finish todo item API

--- server/router.js
+++ server/router.js
@@ -5,11 +5,13 @@ let {User, Item} = require('./models');
 
 mongoose.connect('mongodb://localhost/mytodo', { useNewUrlParser: true,  useUnifiedTopology: true }); 
 
+// Get all users
 router.get('/users', async (req, res) => {
   let users = await User.find();
   res.json(users);
 });
 
+// Get a user
 router.get('/users/:username', async (req, res) => {
   let user = await User.findOne({ username: req.params.username });
   if (user != undefined) {
@@ -20,6 +22,7 @@ router.get('/users/:username', async (req, res) => {
   }
 });
 
+// Add a new user
 router.post('/users', async (req, res) => {
   let newUser = new User({
     username: req.body.username,
@@ -30,6 +33,7 @@ router.post('/users', async (req, res) => {
   res.json(newUser);
 });
 
+// Patch a user
 router.patch('/users/:username', async(req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username });
@@ -47,6 +51,7 @@ router.patch('/users/:username', async(req, res) => {
   }
 });
 
+// Delete a user
 router.delete('/users/:username', async(req, res) => {
   try {
     await User.deleteOne({ username: req.params.username });
@@ -57,17 +62,81 @@ router.delete('/users/:username', async(req, res) => {
   }
 });
 
+// Get all items
 router.get('/todo/', async (req, res) => {
   let todos = await Item.find();
   res.json(todos);
 });
 
-router.post('/todo/', async (req, res) => {
+// Get todo items from a user
+router.get('/todo/:username', async (req, res) => {
+  let user = await User.findOne({ username: req.params.username });
+  let todos = user.todos;
+  res.json(todos);
+});
+
+// Add new item to a user
+router.post('/todo/:username', async (req, res) => {
     let newTodo = new Item({
-      user: req.body.user,
       content: req.body.content,
       done: req.body.done
     });
+
+    let user = await User.findOne({ username: req.params.username });
+    user.todos.push(newTodo);
+    await user.save();
+    res.json(newTodo);
 }); 
 
+// Patch todo item
+router.patch('/todo/:username/:id', async (req, res) => {
+  // Search for user
+  let user;
+  try {
+    user = await User.findOne({ username: req.params.username });
+  } catch {
+    res.status(404);
+    res.json({ "response": "User not found!" });
+  }
+  // Search for todo item
+  try {
+    let item = user.todos.id(req.params.id);
+    // Change item content if supplied
+    if (req.body.content) {
+      item.content = req.body.content;
+    }
+    if (typeof(req.body.done) == 'boolean') {
+      item.done = req.body.done;
+    }
+    // Parents needs to be saved, not the subdocument
+    await user.save();
+    res.json(item);
+
+  } catch {
+    res.status(404);
+    res.json({"response": "Item not found!"});
+  }
+  
+});
+
+// Delete todo item
+router.delete('/todo/:username/:id', async (req, res) => {
+  // Search for user
+  let user;
+  try {
+    user = await User.findOne({ username: req.params.username });
+  } catch {
+    res.status(404);
+    res.json({ "response": "User not found!" });
+  }
+
+  // Delete item
+  try {
+    await user.todos.id(req.params.id).remove();
+    user.save();
+    res.status(204).json();
+  } catch {
+    res.status(404).json({"response": "Item not found!"});
+  }
+})
 module.exports = router;
